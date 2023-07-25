25fb27083cbf9d41b1ebecebd38b6112f34e976b
lhshs <hs.lion@gmail.com>
2023-07-25 16:35:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df = pd.read_csv(mydataset)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
+server = app.server
 
 app.layout = html.Div([
     
